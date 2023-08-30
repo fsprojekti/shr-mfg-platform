cbc837ec3d8066411e77aa209abfbc295a1eadaf
cornflow <marko.corn@gmail.com>
2023-08-30 15:45:07 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+//URL warehouse
+let warehouse="";
+//URL manufacturers
+let manufacturers=[];
+//URL packages
+let packages=[];
+//URL master transport
+let transport="";
+//URL robot cars
+let robotCars=[];
