2226611f749e15498376f16474e91d406e8ac15b
cornflow <marko.corn@gmail.com>
2023-08-31 13:03:40 +0200

    Update config.js

--- config.js
+++ config.js
@@ -8,3 +8,9 @@ let packages=[];
 let transport="";
 //URL robot cars
 let robotCars=[];
+
+//CPL token contract address
+let contractCPLToken="0xBc0256e34B46C7Cf4FC4c30694f1D775e8A16185";
+
+//Capacity Pool contract address
+let contractCapacityPool="0x4d50493e1172e8C1D5D8993486dAcE37E43beCf9";
